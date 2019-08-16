67f6990502694ebad4addb3460081c0b8b8dec5d
Jayin Ton <tonjayin@gmail.com>
2019-08-16 15:35:06 +0800

    UPDATE

---
+++ View/Common/ueditor.php
@@ -0,0 +1,13 @@
+<script type="text/javascript" charset="utf-8"
+        src="{$config_siteurl}statics/admin/ueditor/ueditor.config.js"></script>
+<script type="text/javascript" charset="utf-8"
+        src="{$config_siteurl}statics/admin/ueditor/ueditor.all.min.js"></script>
+<script type="text/javascript" charset="utf-8"
+        src="{$config_siteurl}statics/admin/ueditor/lang/zh-cn/zh-cn.js"></script>s
+<script>
+    var _UEDITOR_CONFIG = {
+        rowspacingtop: ['0', '5', '10', '15', '20', '25'],
+        rowspacingbottom: ['0', '5', '10', '15', '20', '25'],
+        lineheight: ['0', '1', '1.5','1.75','2', '3', '4', '5']
+    }
+</script>
--- View/Demo/demo_edit_car.php
+++ View/Demo/demo_edit_car.php
@@ -3,24 +3,13 @@
 <!--纯前端实现-->
 
 <block name="content">
-    <!--  引入vue，仅作示例，生产环境请勿引入第三方  -->
-    <script src="https://cdn.bootcss.com/vue-i18n/8.12.0/vue-i18n.min.js"></script>
-    <script type="text/javascript" charset="utf-8" src="{$config_siteurl}statics/admin/ueditor/ueditor.config.js"></script>
-    <script type="text/javascript" charset="utf-8" src="{$config_siteurl}statics/admin/ueditor/ueditor.all.min.js"></script>
-    <script type="text/javascript" charset="utf-8" src="{$config_siteurl}statics/admin/ueditor/lang/zh-cn/zh-cn.js"></script>
-    <script>
-        var ueditor_config = {
-            rowspacingtop: ['0', '5', '10', '15', '20', '25'],
-            rowspacingbottom: ['0', '5', '10', '15', '20', '25'],
-            lineheight: ['0', '1', '1.5','1.75','2', '3', '4', '5']
-        }
-    </script>
+
     <div id="app" style="padding: 8px;height: 100%;" v-cloak>
         <el-card>
             <h2>切换示例（Vue-i18n）</h2>
 
             <div style="margin-top: 10px;">
-                <el-form ref="form" label-width="120px" >
+                <el-form ref="form" label-width="120px">
                     <el-form-item label="VIN">
                         <el-input v-model="form.vin"></el-input>
                     </el-form-item>
@@ -57,7 +46,7 @@
                     </el-tabs>
 
 
-                    <el-form-item >
+                    <el-form-item>
                         <el-button type="primary" @click="submit">保存</el-button>
                     </el-form-item>
 
@@ -73,26 +62,21 @@
 
 
 <block name="footer">
-    <script>
+    <!-- 引入UEditor   -->
+    <include file="Common/ueditor"/>
 
+    <script>
         $(document).ready(function () {
             //语言列表
             var _LANG_LIST = JSON.parse('{:json_encode($data["langList"])}') || []
             var _UEDITOR_LIST = {}
-            for(var i=0; i< _LANG_LIST.length;i++){
+            for (var i = 0; i < _LANG_LIST.length; i++) {
+                //初始化UEidtor
                 var lang = _LANG_LIST[i]['lang']
-                _UEDITOR_LIST[lang] = UE.getEditor('editor_content_'+lang, ueditor_config);
+                _UEDITOR_LIST[lang] = UE.getEditor('editor_content_' + lang, _UEDITOR_CONFIG);
             }
 
-            Vue.use(VueI18n)
-            // 通过选项创建 VueI18n 实例
-            var i18n = new VueI18n({
-                locale: '中文', // 设置地区
-                fallbackLocale: '中文',
-            })
-
             new Vue({
-                i18n: i18n,//初始化
                 el: '#app',
                 data: {
                     editor: null,
@@ -109,8 +93,8 @@
                         description: '',
                     },
                     form: {
-                        id:'{:I("get.id")}',
-                        vin:'',
+                        id: '{:I("get.id")}',
+                        vin: '',
                         model: {},
                         year: '',
                         transmission: '0',
@@ -121,12 +105,12 @@
                     //获取详情
                     getDetail: function () {
                         var that = this;
-                        var url = '/Translate/Demo/getCarDetail?id='+this.form.id
-                        that.httpGet(url, {}, function(res){
-                            if(res.status){
+                        var url = '/Translate/Demo/getCarDetail?id=' + this.form.id
+                        that.httpGet(url, {}, function (res) {
+                            if (res.status) {
                                 that.form = res.data;
 
-                                for(var i=0; i< _LANG_LIST.length;i++){
+                                for (var i = 0; i < _LANG_LIST.length; i++) {
                                     var lang = _LANG_LIST[i]['lang']
                                     var editor = _UEDITOR_LIST[lang]
 
@@ -139,15 +123,15 @@
                                         editor.setContent(that.form['description'][lang]);
                                     }.bind(this, editor, lang))
                                 }
-                            }else{
+                            } else {
                                 layer.msg(res.msg, {time: 1000});
                             }
                         });
                     },
-                    submit: function(){
+                    submit: function () {
                         var that = this;
 
-                        for(var i=0; i< _LANG_LIST.length;i++){
+                        for (var i = 0; i < _LANG_LIST.length; i++) {
                             var lang = _LANG_LIST[i]['lang']
                             that.form['description'][lang] = _UEDITOR_LIST[lang].getContent();
                         }
@@ -155,34 +139,20 @@
                         var url = '{:U("Translate/Demo/doAddEditCar")}';
                         var data = that.form;
 
-                        that.httpPost(url, data, function(res){
-                            if(res.status){
-                                layer.msg(res.msg, {time: 1000}, function(){});
-                            }else{
+                        that.httpPost(url, data, function (res) {
+                            if (res.status) {
+                                layer.msg(res.msg, {time: 1000}, function () {
+                                });
+                            } else {
                                 layer.msg(res.msg, {time: 1000});
                             }
                         });
                     }
                 },
-                watch: {
-                    current_lang: function(newValue, oldValue){
-                        var that = this
-                        //当前
-                        if(newValue == 0 || newValue == ''){
-                            return
-                        }
-                        console.log('语言切换为==>'+oldValue +' -> '+newValue)
-                        this.$message.success('语言切换为==>'+newValue)
-                        // this.getDictionaryList(function(){
-                        //
-                        // }.bind(this))
-                        that.$i18n.locale = newValue
-                    },
-                },
+                watch: {},
                 mounted: function () {
                     var that = this
-                    if(that.form.id){
-                        console.log(that.form.id)
+                    if (that.form.id) {
                         that.getDetail()
                     }
                 }
