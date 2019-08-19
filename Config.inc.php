ce0efbceb85d576afa5453c18973ef92430ea445
Jayin Ton <tonjayin@gmail.com>
2019-08-19 11:07:41 +0800

    v1.0.0.0

--- Config.inc.php
+++ Config.inc.php
@@ -6,23 +6,23 @@
 
 return array(
 	//模块名称
-	'modulename' => '多语言',
+	'modulename' => '多语言管理',
 	//图标
 	'icon' => 'https://dn-coding-net-production-pp.qbox.me/e57af720-f26c-4f3b-90b9-88241b680b7b.png',
 	//模块简介
-	'introduce' => '多语言',
+	'introduce' => '多语言管理',
 	//模块介绍地址
 	'address' => 'http://doc.ztbcms.com/module/cron/',
 	//模块作者
-	'author' => 'ZTBCMS',
+	'author' => 'yezhilie,jayin',
 	//作者地址
 	'authorsite' => 'http://www.ztbcms.com',
 	//作者邮箱
 	'authoremail' => 'admin@ztbcms.com',
 	//版本号，请不要带除数字外的其他字符
-	'version' => '0.1.0.2',
+	'version' => '1.0.0.0',
 	//适配最低版本，
-	'adaptation' => '3.7.2.10',
+	'adaptation' => '3.8.1.0',
 	//签名
 	'sign' => 'd98e19e9a5eb746f5c5fe5309b3c806e',
 	//依赖模块
